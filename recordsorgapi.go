44fb204f4b93ec18375425fcab2422056cd78fb4
Simon Tucker <brotherlogic@gmail.com>
2022-03-10 17:43:05 -0800

    Support the cache

---
+++ recordsorgapi.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	rcpb "github.com/brotherlogic/recordcollection/proto"
+	"golang.org/x/net/context"
+)
+
+//ClientUpdate on an updated record
+func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
+	cache, err := s.loadCache(ctx)
+	if err != nil {
+		return &rcpb.ClientUpdateResponse{}, err
+	}
+
+	record, err := s.loadRecord(ctx, req.GetInstanceId())
+	cache.Cache[req.GetInstanceId()] = s.buildCache(record)
+
+	return &rcpb.ClientUpdateResponse{}, s.saveCache(ctx, cache)
+}

f88addd9ead502fbb9e280554bae7ecd5503c8fc
Simon Tucker <brotherlogic@gmail.com>
2022-03-26 12:46:58 -0700

    Fix CLI

--- recordsorg_cli/cli.go
+++ recordsorg_cli/cli.go
@@ -44,22 +44,22 @@ func main() {
 		getFlags := flag.NewFlagSet("get", flag.ExitOnError)
 		var id = getFlags.Int("id", -1, "The name of the budgorget")
 		if err := getFlags.Parse(os.Args[2:]); err == nil {
-		ctx2, cancel2 := utils.ManualContext("recordcollectioncli-"+os.Args[1], time.Hour)
-		defer cancel2()
+			ctx2, cancel2 := utils.ManualContext("recordcollectioncli-"+os.Args[1], time.Hour)
+			defer cancel2()
 
-		conn2, err := utils.LFDialServer(ctx2, "recordcollection")
-		if err != nil {
-			log.Fatalf("Cannot reach rc: %v", err)
-		}
-		defer conn2.Close()
+			conn2, err := utils.LFDialServer(ctx2, "recordcollection")
+			if err != nil {
+				log.Fatalf("Cannot reach rc: %v", err)
+			}
+			defer conn2.Close()
 
-		registry := pbrc.NewRecordCollectionServiceClient(conn2)
-		ids, err := registry.QueryRecords(ctx2, &pbrc.QueryRecordsRequest{Query: &pbrc.QueryRecordsRequest_All{All: true}})
-		if err != nil {
-			log.Fatalf("Bad query: %v", err)
-		}
+			registry := pbrc.NewRecordCollectionServiceClient(conn2)
+			ids, err := registry.QueryRecords(ctx2, &pbrc.QueryRecordsRequest{Query: &pbrc.QueryRecordsRequest_All{All: true}})
+			if err != nil {
+				log.Fatalf("Bad query: %v", err)
+			}
 
-		sclient := pbrc.NewClientUpdateServiceClient(conn)
+			sclient := pbrc.NewClientUpdateServiceClient(conn)
 			log.Printf("PING %v", *id)
 			ctx, cancel = utils.ManualContext("recordsorg-cli-fullping", time.Minute*30)
 			_, err = sclient.ClientUpdate(ctx, &pbrc.ClientUpdateRequest{InstanceId: int32(*id)})
@@ -68,7 +68,6 @@ func main() {
 				log.Fatalf("Error on GET: %v", err)
 			}
 		}
-	}
 	case "fullping":
 		ctx2, cancel2 := utils.ManualContext("recordcollectioncli-"+os.Args[1], time.Hour)
 		defer cancel2()
